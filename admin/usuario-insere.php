350674d605c972cea805ece7a3bbc5b4387e5f94
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-04 15:14:46 -0300

    Update usuario-insere.php

--- admin/usuario-insere.php
+++ admin/usuario-insere.php
@@ -1,34 +1,49 @@
-<?php 
+<?php
+require_once "../src/Database/Conecta.php";
 require_once "../src/Models/Usuario.php";
+require_once "../src/Services/UsuarioServico.php";
 require_once "../src/Helpers/Utils.php";
 
 // Variável que será usada para montar mensagens de erro personalizadas
 $erro = null;
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+// Inicializando um objeto de serviço para o CRUD dos Usuarios
+$usuarioServico = new UsuarioServico();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 	// Validação do preenchimento dos campos
-	if(
-		empty($_POST['nome']) || empty($_POST['email']) || 
+	if (
+		empty($_POST['nome']) || empty($_POST['email']) ||
 		empty($_POST['senha']) || empty($_POST['tipo'])
-	){
+	) {
 		$erro = "Preencha todos os campos";
 	} else {
-		// Capturando e sanitizando os valores do formulário
-		$nome = Utils::sanitizar($_POST['nome']);
-		$email = Utils::sanitizar($_POST['email'], 'email');
-		$tipo = Utils::sanitizar($_POST['tipo']);
+		try {
+			// Capturando e sanitizando os valores do formulário
+			$nome = Utils::sanitizar($_POST['nome']);
+			$email = Utils::sanitizar($_POST['email'], 'email');
+			$tipo = Utils::sanitizar($_POST['tipo']);
 
-		// Capturando e codificando (gerando um hash) da senha
-		$senha = Utils::codificarSenha($_POST['senha']);
+			// Capturando e codificando (gerando um hash) da senha
+			$senha = Utils::codificarSenha($_POST['senha']);
 
-		// Criando um objeto para um novo usuario com seus dados
-		$novoUsuario = new Usuario($nome, $email, $senha, $tipo);
-		
-		// Teste seu método dump AQUI passando o objeto $novoUsuario
-		Utils::dump($novoUsuario);
+			// Criando um objeto para um novo usuario com seus dados
+			$novoUsuario = new Usuario($nome, $email, $senha, $tipo);
+
+			// Executar o serviço e passar os novos dados
+			$usuarioServico->inserir($novoUsuario);
+
+			header("location:usuarios.php");
+			exit;
+		} catch (Throwable $e) {
+			/* Se alguma ação dentro do try falhar,
+			o PHP vai lançar (usando a classe Throwable) um
+			erro/exceção. Ao usar o parâmetro "e" (ou outro nome),
+			temos acesso aos detalhes do que aconteceu. */
+			$erro = "Erro ao inserir usuário. <br>" . $e->getMessage();
+		}
 	}
-
 }
 require_once "../includes/cabecalho-admin.php";
 ?>
@@ -36,27 +51,27 @@ require_once "../includes/cabecalho-admin.php";
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">
-		Inserir novo usuário
+			Inserir novo usuário
 		</h2>
 
 		<!-- O parágrafo abaixo irá aparecer SOMENTE se houver
 		 algum erro. E neste caso, exibirá a mensagem de erro. -->
-		<?php if($erro): ?>
-		<p class="alert alert-danger text-center"> <?=$erro?> </p>
+		<?php if ($erro): ?>
+			<p class="alert alert-danger text-center"> <?= $erro ?> </p>
 		<?php endif; ?>
-				
+
 		<form class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir" autocomplete="off">
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input required value="<?=$_POST['nome'] ?? ''?>" class="form-control" type="text" id="nome" name="nome">
+				<input required value="<?= $_POST['nome'] ?? '' ?>" class="form-control" type="text" id="nome" name="nome">
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input required value="<?=$_POST['email'] ?? ''?>" class="form-control" type="email" id="email" name="email">
+				<input required value="<?= $_POST['email'] ?? '' ?>" class="form-control" type="email" id="email" name="email">
 			</div>
 
 			<div class="mb-3">
@@ -72,15 +87,14 @@ require_once "../includes/cabecalho-admin.php";
 					<option value="admin">Administrador</option>
 				</select>
 			</div>
-			
+
 			<button class="btn btn-primary" id="inserir" name="inserir"><i class="bi bi-save"></i> Inserir</button>
 		</form>
-		
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require_once "../includes/rodape-admin.php";
 ?>
-
