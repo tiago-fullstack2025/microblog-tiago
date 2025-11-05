f7995514ccf2a67290c5a18cd9b87156d889db43
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-05 16:29:03 -0300

    Adiciona método verificarSenha

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -24,6 +24,22 @@ class Utils {
     }
 
 
+    /* Ao chamar o método verificarSenha, passamos pra ele
+    a senha digitada no formulário e a senha existente no banco. */
+    public static function verificarSenha(
+        string $senhaDigitadaNoFormulario, string $senhaArmazenadaNoBanco
+    ):string {
+        /* Usamos o password_verify para COMPARAR as duas senhas. */
+        if( password_verify($senhaDigitadaNoFormulario, $senhaArmazenadaNoBanco) ){
+            // São iguais? Então retorne a mesma senha já existente no banco
+            return $senhaArmazenadaNoBanco;
+        } else {
+            // São diferentes? Então pega a senha digitada e faça um novo hash
+            return self::codificarSenha($senhaDigitadaNoFormulario);
+        }
+    }
+
+
     /* Exercício: crie um método chamado dump, faça ele receber
     um parâmetro chamado $dados, e faça aparecer o var_dump dentro da tag <pre> */
     public static function dump(mixed $dados):void {
