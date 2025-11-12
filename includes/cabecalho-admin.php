c8ba08df6c9ffcd181e029e86ecd44bf6e31564f
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-12 17:43:16 -0300

    Update cabecalho-admin.php

--- includes/cabecalho-admin.php
+++ includes/cabecalho-admin.php
@@ -46,11 +46,11 @@ if(isset($_GET['sair'])) AutenticacaoServico::logout();
                         </li>
 
 
-                       
+                        <?php if( $_SESSION['tipo'] === 'admin' ): ?>
                         <li class="nav-item">
                             <a class="nav-link" href="usuarios.php">Usuários</a>
                         </li>
-
+                        <?php endif; ?>
 
                         <li class="nav-item">
                             <a class="nav-link" href="../index.php" target="_blank">Área pública</a>
