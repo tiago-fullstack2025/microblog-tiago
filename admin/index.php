4d899b1813243e24e122cb01350ace11330c700d
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-12 17:43:13 -0300

    Exibe dados do usuário logado

--- admin/index.php
+++ admin/index.php
@@ -10,10 +10,10 @@ require_once "../includes/cabecalho-admin.php";
 <article class="p-5 my-4 rounded-3 bg-white shadow">
     <div class="container-fluid py-1">
 
-        <h2 class="display-4">Olá!</h2>
+        <h2 class="display-4">Olá <?= $_SESSION['nome']?> !</h2>
 
         <p class="fs-5">Você está no <b>painel de controle e administração</b> do site Microblog e seu <b>nível de acesso</b> é
-            <span class="badge bg-dark"> admin|editor </span>.
+            <span class="badge bg-dark"> <?=$_SESSION['tipo']?> </span>.
         </p>
         <hr class="my-4">
 
@@ -28,10 +28,12 @@ require_once "../includes/cabecalho-admin.php";
                 Notícias
             </a>
 
+            <?php if( $_SESSION['tipo'] === 'admin' ): ?>
             <a class="btn btn-dark bg-gradient btn-lg" href="usuarios.php">
                 <i class="bi bi-people"></i> <br>
                 Usuários
             </a>
+            <?php endif; ?>
 
 
         </div>
