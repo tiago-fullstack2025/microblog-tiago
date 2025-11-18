50a2df6bee330046e819d7dfdbb4827ddc3bb104
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-18 17:29:27 -0300

    Refatora método pra uso do JOIN

--- src/Services/NoticiaServico.php
+++ src/Services/NoticiaServico.php
@@ -10,7 +10,15 @@ class NoticiaServico {
 
     // Versão básica (provisória)
     public function buscar():array {
-        $sql = "SELECT * FROM noticias ORDER BY data DESC";
+        $sql = "SELECT  
+                    noticias.id,
+                    noticias.titulo,
+                    noticias.data,
+                    usuarios.nome AS autor
+                FROM noticias JOIN usuarios
+                ON noticias.usuario_id = usuarios.id                
+                ORDER BY data DESC";
+
         $consulta = $this->conexao->query($sql);
         return $consulta->fetchAll();
     }
