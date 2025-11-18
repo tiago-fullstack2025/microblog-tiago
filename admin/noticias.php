11f9c6cff260ea4eaec1dce7c96bcb2b27ebbf46
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-18 16:22:54 -0300

    Update noticias.php

--- admin/noticias.php
+++ admin/noticias.php
@@ -1,10 +1,22 @@
 <?php 
+require_once "../src/Database/Conecta.php";
 require_once "../src/Helpers/Utils.php";
+require_once "../src/Services/NoticiaServico.php";
 require_once "../src/Services/AutenticacaoServico.php";
 AutenticacaoServico::exigirLogin();
 
-require_once "../includes/cabecalho-admin.php";
+$erro = null;
+$noticias = [];
+$noticiaServico = new NoticiaServico();
 
+try {
+	$noticias = $noticiaServico->buscar();
+	Utils::dump($noticias);
+} catch (Throwable $e) {
+	$erro = "Erro ao buscar notícias. <br>".$e->getMessage();
+}
+
+require_once "../includes/cabecalho-admin.php";
 ?>
 
 
@@ -12,6 +24,10 @@ require_once "../includes/cabecalho-admin.php";
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
 		
 		<h2 class="text-center">Notícias <span class="badge bg-dark">X</span></h2>
+
+		<?php if ($erro): ?>
+			<p class="alert alert-danger text-center"> <?= $erro ?> </p>
+		<?php endif; ?>
 
 		<p class="text-center mt-5">
 			<a class="btn btn-primary" href="noticia-insere.php">
