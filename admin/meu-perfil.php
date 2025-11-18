07d9e536c75d7e5b0f50545d90adccaa99fb34de
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-18 15:23:55 -0300

    Programa Meu Perfil

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,7 +1,55 @@
 <?php
+require_once "../src/Database/Conecta.php";
+require_once "../src/Models/Usuario.php";
 require_once "../src/Helpers/Utils.php";
+require_once "../src/Services/UsuarioServico.php";
 require_once "../src/Services/AutenticacaoServico.php";
 AutenticacaoServico::exigirLogin();
+
+// Inicialização
+$erro = null;
+$usuarioServico = new UsuarioServico();
+
+try {
+	// Buscar a partir do id do usuário logado
+	$dados = $usuarioServico->buscarPorId($_SESSION['id']);
+	if(!$dados) $erro = "Usuário não encontrado";
+} catch (Throwable $e) {
+	$erro = "Erro ao buscar usuário. <br>".$e->getMessage();
+}
+
+// Detectar se o formulário foi acionando para atualizar o usuário
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+	if(empty($_POST['nome']) || empty($_POST['email'])){
+		$erro = "Nome e e-mail são obrigatórios";
+	} else {
+		try {
+			$nome = Utils::sanitizar($_POST['nome']);
+			$email = Utils::sanitizar($_POST['email'], 'email');
+
+			/* Se o campo senha estiver vazio, manter a senha existente.
+			Caso contrário, verifique as senhas (digitada no form e a do banco) */
+			$senha = empty($_POST['senha']) 
+						? $dados['senha'] 
+						: Utils::verificarSenha($_POST['senha'], $dados['senha']);
+
+			// Montando um objeto com os dados do meu perfil (pessoa logada)
+			$usuario = new Usuario($nome, $email, $senha, $_SESSION['tipo'], $_SESSION['id']);
+
+			// Executar o serviço para atualizar
+			$usuarioServico->atualizar($usuario);
+
+			// Forçando a atualização da variável de sessão
+			$_SESSION['nome'] = $nome;
+
+			// Redireciona para a lista de usuários
+			Utils::redirecionarPara("index.php");
+		} catch (Throwable $e) {
+			$erro = "Erro ao editar usuário. <br>".$e->getMessage();
+		}
+	}
+}
 
 require_once "../includes/cabecalho-admin.php";
 
@@ -15,17 +63,21 @@ require_once "../includes/cabecalho-admin.php";
 			Atualizar meus dados
 		</h2>
 
+		<?php if ($erro): ?>
+			<p class="alert alert-danger text-center"> <?= $erro ?> </p>
+		<?php endif; ?>
+
 		<form class="mx-auto w-75" action="" method="post" id="form-atualizar" name="form-atualizar">
-			<input type="hidden" name="id" value="id do usuário logado">
+			<input type="hidden" name="id" value="<?= $dados['id']  ?>">
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input value="Nome do usuário logado" class="form-control" type="text" id="nome" name="nome">
+				<input value="<?= $dados['nome']  ?>" class="form-control" type="text" id="nome" name="nome">
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input value="email@dousuariologado.com" class="form-control" type="email" id="email" name="email">
+				<input value="<?= $dados['email']  ?>" class="form-control" type="email" id="email" name="email">
 			</div>
 
 			<div class="mb-3">
