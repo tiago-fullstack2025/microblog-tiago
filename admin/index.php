d3f0adfc3844b1b454ffb533321d586ea186aca1
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-18 13:58:32 -0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,7 @@ require_once "../includes/cabecalho-admin.php";
 <article class="p-5 my-4 rounded-3 bg-white shadow">
     <div class="container-fluid py-1">
 
-        <h2 class="display-4">Olá <?= $_SESSION['nome']?> !</h2>
+        <h2 class="display-4">Olá <?= $_SESSION['nome']?>!</h2>
 
         <p class="fs-5">Você está no <b>painel de controle e administração</b> do site Microblog e seu <b>nível de acesso</b> é
             <span class="badge bg-dark"> <?=$_SESSION['tipo']?> </span>.
