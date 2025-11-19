5944dfea57b7a8fa77ca8a8cb0fc31eed90ab8bd
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-19 16:32:33 -0300

    Adiciona método upload

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -1,25 +1,28 @@
 <?php
 // src/Helpers/Utils.php
 
-class Utils {
+class Utils
+{
 
     /* Usamos mixed para sinalizar que o método aceita/retorna
     tipos de dados variados (string, int, array, float etc) */
-    public static function sanitizar(mixed $valor, string $tipoDeSanitizacao = 'texto'):mixed {
-        switch($tipoDeSanitizacao){
+    public static function sanitizar(mixed $valor, string $tipoDeSanitizacao = 'texto'): mixed
+    {
+        switch ($tipoDeSanitizacao) {
             case 'inteiro':
                 return (int) filter_var($valor, FILTER_SANITIZE_NUMBER_INT);
-            
+
             case 'email':
                 return trim(filter_var($valor, FILTER_SANITIZE_EMAIL));
-            
+
             default:
                 return trim(filter_var($valor, FILTER_SANITIZE_SPECIAL_CHARS));
         }
     }
 
 
-    public static function codificarSenha(string $valorSenha):string {
+    public static function codificarSenha(string $valorSenha): string
+    {
         return password_hash($valorSenha, PASSWORD_DEFAULT);
     }
 
@@ -27,10 +30,11 @@ class Utils {
     /* Ao chamar o método verificarSenha, passamos pra ele
     a senha digitada no formulário e a senha existente no banco. */
     public static function verificarSenha(
-        string $senhaDigitadaNoFormulario, string $senhaArmazenadaNoBanco
-    ):string {
+        string $senhaDigitadaNoFormulario,
+        string $senhaArmazenadaNoBanco
+    ): string {
         /* Usamos o password_verify para COMPARAR as duas senhas. */
-        if( password_verify($senhaDigitadaNoFormulario, $senhaArmazenadaNoBanco) ){
+        if (password_verify($senhaDigitadaNoFormulario, $senhaArmazenadaNoBanco)) {
             // São iguais? Então retorne a mesma senha já existente no banco
             return $senhaArmazenadaNoBanco;
         } else {
@@ -42,18 +46,68 @@ class Utils {
 
     /* Exercício: crie um método chamado dump, faça ele receber
     um parâmetro chamado $dados, e faça aparecer o var_dump dentro da tag <pre> */
-    public static function dump(mixed $dados):void {
+    public static function dump(mixed $dados): void
+    {
         echo "<pre>";
         var_dump($dados);
         echo "</pre>";
     }
 
-    public static function redirecionarPara(string $destino):void {
-        header("location:".$destino);
-		exit;
+    public static function redirecionarPara(string $destino): void
+    {
+        header("location:" . $destino);
+        exit;
     }
 
-    public static function formatarData(string $valorData):string {
+    public static function formatarData(string $valorData): string
+    {
         return date("d/m/Y H:i", strtotime($valorData));
+    }
+
+
+    public static function upload(?array $arquivo): void
+    {
+
+        /* Validação inicial, verificando se:
+        - não tem arquivo
+        - não existe alguma referência na área temporária
+        - não for um arquivo que possa/permita envio/upload */
+        if (
+            !$arquivo ||
+            !isset($arquivo["tmp_name"]) ||
+            !is_uploaded_file($arquivo["tmp_name"])
+        ) {
+            throw new Exception("Nenhum arquivo válido foi enviado.");
+        }
+
+        // Definindo uma pasta no servidor/site para receber a imagem enviada
+        $pastaDeDestino = "../images/";
+
+        // Validação dos formatos de imagem
+        $formatosPermitidos = ['image/jpeg', 'image/png', 'image/gif', 'image/svg+xml'];
+
+        // Definindo um tamanho máximo pra imagens
+        $tamanhoMaximo = 2 * 1024 * 1024; // 2MB
+
+        // Detectando o formato REAL do arquivo
+        $formatoDoArquivoEnviado = mime_content_type($arquivo["tmp_name"]);
+
+        // Se o formato NÃO ESTIVER na lista de formatos permitidos
+        if (!in_array($formatoDoArquivoEnviado, $formatosPermitidos)) {
+            throw new Exception("Apenas arquivos JPG, PNG, GIF e SVG são permitidos.");
+        }
+
+        // Se o tamanho do arquivo enviado for acima do máximo
+        if ($arquivo["size"] > $tamanhoMaximo) {
+            throw new Exception("O arquivo é muito grande. Tamanho máximo: 2MB.");
+        }
+
+        // Montando o nome/caminho do arquivo que será guardado na pasta imagens
+        $nomeDoArquivo = $pastaDeDestino . basename($arquivo["name"]);
+
+        // Se NÃO CONSEGUIR executar a função move_uploaded_file, lançar uma exceção
+        if (!move_uploaded_file($arquivo["tmp_name"], $nomeDoArquivo)) {
+            throw new Exception("Erro ao mover o arquivo. Código de erro: " . $arquivo["error"]);
+        }
     }
 }
