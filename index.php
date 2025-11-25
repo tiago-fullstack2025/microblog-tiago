c991c8678cd4bf446e3aac2a17608445856c363c
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-11-25 17:33:09 -0300

    Script com as noticias restantes

--- index.php
+++ index.php
@@ -1,22 +1,42 @@
 <?php
+require_once "src/Database/Conecta.php";
+require_once "src/Services/NoticiaServico.php";
+require_once "src/Helpers/Utils.php";
+
+$erro = null;
+$noticias = [];
+$noticiaServico = new NoticiaServico();
+
+try {
+    $noticias = $noticiaServico->buscarNoticiasParaAreaPublica();
+} catch (Throwable $e) {
+    $erro = "Erro ao buscar notícias. <br>".$e->getMessage(); 
+}
+
 require_once "includes/cabecalho.php";
 ?>
+
+<?php if ($erro): ?>
+			<p class="alert alert-danger text-center"> <?= $erro ?> </p>
+		<?php endif; ?>
 
 <div class="row my-1 mx-md-n1">
 
     <!-- INÍCIO Card -->
 
+    <?php foreach($noticias as $noticia): ?>
     <div class="col-md-6 my-1 px-md-1">
         <article class="card shadow-sm h-100">
-            <a href="noticia.php" class="card-link">
-                <img src="images/abstrato.jpg" class="card-img-top" alt="Imagem de capa do card">
+            <a href="noticia.php?id=<?= $noticia['id'] ?>" class="card-link">
+                <img src="images/<?= $noticia['imagem'] ?>" class="card-img-top" alt="Imagem de capa do card">
                 <div class="card-body">
-                    <h3 class="fs-4 card-title">Título da notícia...</h3>
-                    <p class="card-text">Resumo da notícia...</p>
+                    <h3 class="fs-4 card-title"> <?= $noticia['titulo'] ?> </h3>
+                    <p class="card-text"> <?= $noticia['resumo'] ?> </p>
                 </div>
             </a>
         </article>
     </div>
+    <?php endforeach; ?>
 
     <!-- FIM Card -->
 
